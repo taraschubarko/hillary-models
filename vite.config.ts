319e711adc19216068df473ee49d1cc6a744fde5
tarik <tchubarko@gmail.com>
2026-02-13 15:50:31 +0200

    fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,23 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
+import { htmlPrerender } from 'vite-plugin-html-prerender'
 
 
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
   plugins: [
     inspectAttr(),
     react(),
 
-    // // важливо: вмикаємо ТІЛЬКИ на build
-    // ...(command === 'build'
-    //     ? [
-    //       htmlPrerender({
-    //         staticDir: path.join(__dirname, 'dist'),
-    //         routes: ['/'], // лендінг
-    //       }),
-    //     ]
-    //     : []),
+    // важливо: вмикаємо ТІЛЬКИ на build
+    ...(command === 'build'
+        ? [
+          htmlPrerender({
+            staticDir: path.join(__dirname, 'dist'),
+            routes: ['/'], // лендінг
+          }),
+        ]
+        : []),
   ],
   resolve: {
     alias: {
